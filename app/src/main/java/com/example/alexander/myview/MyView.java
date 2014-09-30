05b24ef4cae92b36ddc09a84eb5bdfd00588bc03
SoloAs <Sunny.Alexander@mail.ru>
2014-09-30 13:27:57 +0400

    Attributes names fixed

--- app/src/main/java/com/example/alexander/myview/MyView.java
+++ app/src/main/java/com/example/alexander/myview/MyView.java
@@ -5,12 +5,16 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by Alexander on 9/30/2014.
  */
 public class MyView extends RelativeLayout {
-    String mMainText, mExtraText;
+
+    private TextView mainText;
+    private TextView extraText;
+
     public MyView(Context context) {
         super(context);
         init(context, null);
@@ -36,8 +40,14 @@ public class MyView extends RelativeLayout {
                 0, 0);
 
         try {
-            mMainText = a.getString(R.styleable.MyView_mainText);
-            mExtraText = a.getString(R.styleable.MyView_extraText);
+            String mMainText = a.getString(R.styleable.MyView_mainText);
+            String mExtraText = a.getString(R.styleable.MyView_extraText);
+
+            mainText = (TextView) findViewById(R.id.custom_view_name);
+            extraText = (TextView) findViewById(R.id.custom_view_work);
+
+            mainText.setText(mMainText);
+            extraText.setText(mExtraText);
 
             invalidate();
             requestLayout();
